1ef32bf10871689e46af53ef320af68e8a7bde04
grailwilly <grailjwi11y@gmail.com>
2022-09-29 23:27:58 +0800

    set article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -22,21 +22,28 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-
-    if @article.update
-      redirect @article
+    if @article.update(article_params)
+      redirect_to @article
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
