1ad44a6fe1f0d9dc322f2b889c0f4392842fca23
grailwilly <grailjwi11y@gmail.com>
2022-09-30 11:17:35 +0800

    add delete method

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,14 +1,25 @@
 class CommentsController < ApplicationController
+  before_action :set_article
+  
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
 
     redirect_to article_path(@article)
+  end
+
+  def destroy
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+      redirect_to article_path(@article), status: :see_other
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:commenter, :body, :status)
+  end
+
+  def set_article
+    @article = Article.find(params[:article_id])
   end
 end
