98919f3493305e941050c5ac2a53974b516f3c64
grailwilly <grailjwi11y@gmail.com>
2022-11-09 10:54:08 +0800

    set default role

--- sales/app/models/user.rb
+++ sales/app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   belongs_to :role
+  before_create :set_default_role
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def set_default_role
+    self.role ||= Role.find_by_name('user')
+  end
 end
